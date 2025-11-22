632d30c62a355e9995937dd067e49f4663ee53c8
Divarizky <divarizky28@gmail.com>
2025-11-22 16:08:02 +0700

    chore: menambahkan dependensi

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mini App: Dicoding Event',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
       home: const MyHomePage(title: 'Dicoding Event'),
     );
   }
@@ -29,19 +26,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(widget.title, style: TextStyle(color: Colors.white)),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          ],
+          children: <Widget>[],
         ),
       ),
     );
